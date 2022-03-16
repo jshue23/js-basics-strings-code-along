db6300b47219f2a38c7af8f6141d08585b9d2159
shueijo <jshue23@gmail.com>
2022-03-16 14:47:38 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = 'Default';
+let welcomeMessage = 'Welcome to Flatbook, ${Default}!';
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ${DEFAULT}!'; 
+let shortGreeting = 'Welcome, ${D}!';
+
